17b276c2b772e70ce9e11969789a3eac5e06b85d
Yair Cohen <56881475+yaircohendev@users.noreply.github.com>
2023-04-17 16:11:09 +0300

    Fix lib execution with npx (#16)
    * fix execution of library
    
    * fix output console log

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,16 +2,12 @@ import { TSFilePlugin } from "../../plugins/ts-file";
 import ImportsPlugin from "../../plugins/imports";
 import { AnalyzerPlugin } from "../shared/models/plugin.model";
 import { Type } from "../shared/models/general.model";
-import ConsoleLogFinalizer from "../../plugins/console-log-finalizer";
 import UsedHTMLTags from "../../plugins/used-html-tags";
 import ChartJSPlugin from "../../plugins/chartsjs-plugin";
-
-export const MAIN_REPOS_FOLDER_PATH = "_repo";
 
 export const DEFAULT_PLUGINS: Type<AnalyzerPlugin>[] = [
   TSFilePlugin,
   ImportsPlugin,
-  ConsoleLogFinalizer,
   UsedHTMLTags,
   ChartJSPlugin,
 ];
