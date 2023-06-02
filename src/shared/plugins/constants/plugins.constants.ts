dd18183f2c6a1e1675583ec9cdd25d571b0e3832
Yair Cohen <yair.cohen@coralogix.com>
2023-06-02 22:39:35 +0300

    fix imports

--- src/shared/plugins/constants/plugins.constants.ts
+++ src/shared/plugins/constants/plugins.constants.ts
@@ -3,11 +3,11 @@ import OpensearchFinalizer from "../opensearch-plugin";
 import CoralogixPlugin from "../coralogix-plugin";
 import { BaseAnalyzerPlugin } from "../../models/plugins";
 import { Type } from "../../models/general.model";
-import ImportsPlugin from "../../../../lib/shared/plugins/imports";
-import TSFilePlugin from "../../../../lib/shared/plugins/ts-file";
 import UsedHTMLTags from "../used-html-tags";
 import DependenciesPlugin from "../dependencies-plugin";
 import ChartJSPlugin from "../chartsjs-plugin";
+import ImportsPlugin from "../imports";
+import TSFilePlugin from "../ts-file";
 
 export const supportedPlugins: Record<string, Type<BaseAnalyzerPlugin>> = {
   angular: AngularPlugin,
