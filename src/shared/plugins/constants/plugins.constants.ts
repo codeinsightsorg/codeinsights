26dc7094128189eaa2c3407419b26b7b9107c0ec
Yair Cohen <yair.cohen@coralogix.com>
2023-06-02 17:42:00 +0300

    remove console plugin

--- src/shared/plugins/constants/plugins.constants.ts
+++ src/shared/plugins/constants/plugins.constants.ts
@@ -8,13 +8,11 @@ import TSFilePlugin from "../../../../lib/shared/plugins/ts-file";
 import UsedHTMLTags from "../used-html-tags";
 import DependenciesPlugin from "../dependencies-plugin";
 import ChartJSPlugin from "../chartsjs-plugin";
-import ConsoleLogFinalizer from "../console-log-finalizer";
 
 export const supportedPlugins: Record<string, Type<BaseAnalyzerPlugin>> = {
   angular: AngularPlugin,
   opensearch: OpensearchFinalizer,
   coralogix: CoralogixPlugin,
-  console: ConsoleLogFinalizer,
   charts: ChartJSPlugin,
   imports: ImportsPlugin,
   ts: TSFilePlugin,
