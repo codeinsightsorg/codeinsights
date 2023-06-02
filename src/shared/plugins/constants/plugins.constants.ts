e7b66cc559d57a22e45fa623720d14a676e6555f
Yair Cohen <yair.cohen@coralogix.com>
2023-06-02 12:45:41 +0300

    add plugin ids to the plugins list

--- src/shared/plugins/constants/plugins.constants.ts
+++ src/shared/plugins/constants/plugins.constants.ts
@@ -1,11 +1,23 @@
 import AngularPlugin from "../angular-plugins";
 import OpensearchFinalizer from "../opensearch-plugin";
 import CoralogixPlugin from "../coralogix-plugin";
-import { BaseAnalyzerPlugin } from "../../models/plugins/plugin.model";
+import { BaseAnalyzerPlugin } from "../../models/plugins";
 import { Type } from "../../models/general.model";
+import ImportsPlugin from "../../../../lib/shared/plugins/imports";
+import TSFilePlugin from "../../../../lib/shared/plugins/ts-file";
+import UsedHTMLTags from "../used-html-tags";
+import DependenciesPlugin from "../dependencies-plugin";
+import ChartJSPlugin from "../chartsjs-plugin";
+import ConsoleLogFinalizer from "../console-log-finalizer";
 
 export const supportedPlugins: Record<string, Type<BaseAnalyzerPlugin>> = {
   angular: AngularPlugin,
   opensearch: OpensearchFinalizer,
   coralogix: CoralogixPlugin,
+  console: ConsoleLogFinalizer,
+  charts: ChartJSPlugin,
+  imports: ImportsPlugin,
+  ts: TSFilePlugin,
+  html: UsedHTMLTags,
+  dependencies: DependenciesPlugin,
 };
