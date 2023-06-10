cc926571ed8732eb22abd52a379c212cad2bd714
Yair Cohen <yair.cohen@coralogix.com>
2023-06-10 12:27:42 +0300

    add exports to plugin class

--- src/index.ts
+++ src/index.ts
@@ -13,4 +13,5 @@ init().then();
 export { ConfigModel, ConfigPluginModel } from "./shared/models/config.model";
 export * from "./shared/models/plugins/index";
 export * from "./shared/models/analyze.model";
+export * from "./modules/plugins/index";
 export { BasePlugin } from "./modules/parser/plugin-parsers/analyze-plugin";
